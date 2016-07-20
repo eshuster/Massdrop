abd1e46e4a595c3654643065e5578c162dcbc5e6
eshuster <eshuster@kontrolscan.com>
2016-07-20 15:31:31 -0700

    Fix spelling

--- app/models/job.js
+++ app/models/job.js
@@ -1,10 +1,7 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-// var bcrypt     = require('bcrypt-nodejs');
 
-// user schema
 var JobSchema   = new Schema({
-  // name: String,
   status: { type: String, required: true, index: { unique: true }},
 });
 
